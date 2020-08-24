d4656dafd93640207cc3d06edb5443ddf75df23a
PTCSZOOM <70174866+PTCSZOOM@users.noreply.github.com>
2020-08-24 18:27:03 -0400

    Create process.js

---
+++ process.js
@@ -0,0 +1,11 @@
+<script>
+function getURL() {
+  var links = ["https://parktudor-org.zoom.us/j/81463027750?pwd=aW9ib1UvRW1TczZWRko0dmJ0ek1DQT09", "https://parktudor-org.zoom.us/j/81888256555?pwd=OEhkNFFRemJFSXhCdDE5cGpjUkhGQT09", "https://parktudor-org.zoom.us/j/82763173963?pwd=Z1BCcHJxY1h2Nm5KUlo0bmF2MVRmdz09", "https://parktudor-org.zoom.us/j/84142823489?pwd=REpENXlBcE9MNEVXTkQ3NExtMXlqQT09", "https://parktudor-org.zoom.us/j/89076918752?pwd=aktuOXZLR3ZjcjJjNTJyNWU5SFJ5UT09"]
+  var d = new Date();
+  var n = d.getDay()
+  
+  if(n>=1 and n<=5)
+    return links[n-1]
+  else return links[0]
+}
+</script>
