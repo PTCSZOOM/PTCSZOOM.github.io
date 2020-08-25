0843631dfa902c9ba47f292d130ff76047101b18
PTCSZOOM <70174866+PTCSZOOM@users.noreply.github.com>
2020-08-24 20:16:10 -0400

    Update process.js

--- process.js
+++ process.js
@@ -1,5 +1,4 @@
-  <script type="text/javascript">
-  
+ 
   function getURL() {
   var links = ["https://parktudor-org.zoom.us/j/81463027750?pwd=aW9ib1UvRW1TczZWRko0dmJ0ek1DQT09", "https://parktudor-org.zoom.us/j/81888256555?pwd=OEhkNFFRemJFSXhCdDE5cGpjUkhGQT09", "https://parktudor-org.zoom.us/j/82763173963?pwd=Z1BCcHJxY1h2Nm5KUlo0bmF2MVRmdz09", "https://parktudor-org.zoom.us/j/84142823489?pwd=REpENXlBcE9MNEVXTkQ3NExtMXlqQT09", "https://parktudor-org.zoom.us/j/89076918752?pwd=aktuOXZLR3ZjcjJjNTJyNWU5SFJ5UT09"];
   var d = new Date();
@@ -12,4 +11,3 @@
     return links[0];
   }
 }
-</script>
